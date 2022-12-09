a195f23ec8587d90cd71f38cff181d4346f63aac
hyoinandout <hobbyhyoin@naver.com>
2022-12-09 13:49:18 +0900

    refactor: lint

--- meal.py
+++ meal.py
@@ -4,14 +4,16 @@ class Meal:
     kind_of_meal: str
     menu: str
 
+
 class MealWrapper:
     meal: Meal
 
     def __init__(self) -> None:
         self.meal = Meal()
 
-kind_of_meals = ["조식","중식","석식"]
-kind_of_restaurants = ["제1학생회관1층","제1학생회관2층","제2학생회관1층"]
 
-slot_endpoints = [(3,13),(13,23),(23,30)]
-slot_filenames_postfix = ['_b_kor.json','_l_kor.json','_d_kor.json']
+kind_of_meals = ["조식", "중식", "석식"]
+kind_of_restaurants = ["제1학생회관1층", "제1학생회관2층", "제2학생회관1층"]
+
+slot_endpoints = [(3, 13), (13, 23), (23, 30)]
+slot_filenames_postfix = ["_b_kor.json", "_l_kor.json", "_d_kor.json"]
