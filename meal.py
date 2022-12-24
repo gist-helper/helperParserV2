bc4d6c8fdcc49ee3035ef9a901e1f2110a0ad81b
hyoinandout <hobbyhyoin@naver.com>
2022-12-24 16:31:48 +0900

    chore: sync with current version

--- meal.py
+++ meal.py
@@ -13,7 +13,15 @@ class MealWrapper:
 
 
 kind_of_meals = ["조식", "중식", "석식"]
+kind_of_meals_eng = ["Breakfast", "Lunch", "Dinner"]
+
 kind_of_restaurants = ["제1학생회관1층", "제1학생회관2층", "제2학생회관1층"]
+kind_of_restaurants_eng = [
+    "Student Union Bldg.1 1st floor",
+    "Student Union Bldg.1 2nd floor",
+    "Student Union Bldg.2 1st floor",
+]
 
 slot_endpoints = [(3, 13), (13, 23), (23, 30)]
 slot_filenames_postfix = ["_b_kor.json", "_l_kor.json", "_d_kor.json"]
+slot_filenames_postfix_eng = ["_b_eng.json", "_l_eng.json", "_d_eng.json"]
