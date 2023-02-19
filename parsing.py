aebdc0cc2b45ca7695e49db36f9bf67e542dad0d
leayeaeun <dldpdms115@naver.com>
2023-02-19 20:50:30 +0900

    1학 영어 파싱

--- parsing.py
+++ parsing.py
@@ -24,7 +24,8 @@ def parsing(excel_path: str, bldgType: int, langType: int) -> list:
     sheet = pd.read_excel(excel_path, sheet_name=langType, 
                           index_col=None, header=None, names=col, engine='openpyxl')
     parsing_result = []
-    if bldgType == 0: #1학
+
+    if bldgType == 0 & langType == 0: #1학 한글 엑셀
         xl = pd.read_excel(excel_path, sheet_name=None,
                            index_col=None, header=None, names=col, engine='openpyxl')
         last_sheet = list(xl.keys())[-2]
@@ -41,7 +42,21 @@ def parsing(excel_path: str, bldgType: int, langType: int) -> list:
                 parsing_result.append(meal.__dict__)        
 
         return parsing_result
+    
 
+    if bldgType == 0 & langType == 1: #1학 영어 엑셀
+        for (dateType, date_str) in enumerate(DATE_1st):
+            date_sheet = sheet[date_str]
+            date = parsing_date(time_index, date_sheet)
+            for kindType in [BREAKFAST, LUNCH, DINNER]:
+                kind = MEAL_KIND[langType][kindType]
+                menu, special = parsing_meal[kindType](endpoint, date_sheet, langType, dateType, bldgType)
+                meal = Meal(bldgType, langType, dateType, kindType, 
+                        bldg, date, kind, menu, special)
+                parsing_result.append(meal.__dict__) 
+
+        return parsing_result
+            
     elif bldgType == 2: #2학
         for (dateType, date_str) in enumerate(DATE_2nd):
             date_sheet = sheet[date_str]
@@ -75,6 +90,7 @@ if __name__ == "__main__":
     Mode = 0
     if Mode == 0:
         excel_path_1 = "./1학생회관.xlsx"
+        excel_path_1_1 = "./eng1학생회관.xlsx"
         excel_path_2 = "./2학생회관.xlsx"
         parsing_result_1 = []
         parsing_result_2 = []
@@ -82,19 +98,22 @@ if __name__ == "__main__":
         print("parsing xlsx to json...")
         if exists(excel_path_1):
             parsing_result_1.extend(parsing(excel_path_1, BLDG1_1ST, KOR)) #1학 1층, 한글
-            #parsing_result.extend(parsing(excel_path, BLDG1_1ST, ENG)) #1학 1층, 영어
+        if exists(excel_path_1_1):
+            parsing_result_1.extend(parsing(excel_path_1_1, BLDG1_1ST, ENG)) #1학 1층, 영어
         if exists(excel_path_2):
             parsing_result_2.extend(parsing(excel_path_2, BLDG2_1ST, KOR)) #2학 1층, 한글
             parsing_result_2.extend(parsing(excel_path_2, BLDG2_1ST, ENG)) #2학 1층, 영어
         print("-------------------------------------------------")
+
         if len(sys.argv) == 1:
             # no url, save to local as json
             print("-------------------------------------------------")
             print("saving json...")
-            if exists(excel_path_1):
+            if exists(excel_path_1) & exists(excel_path_1_1):
                 jsonFile = open("./1nd1floor_meal.json", "w", encoding="utf-8")
                 json.dump(parsing_result_1, jsonFile, indent=4,
-                    ensure_ascii=False, cls=ComplexEncoder)        
+                    ensure_ascii=False, cls=ComplexEncoder)
+    
             if exists(excel_path_2):
                 jsonFile = open("./2nd1floor_meal.json", "w", encoding="utf-8")
                 json.dump(parsing_result_2, jsonFile, indent=4,
